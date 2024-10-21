cd0be71340fb97ca1c7892fcd80cb04b35fe1cf8
Rishabh0411 <rishabh.sharma041108@gmail.com>
2024-10-21 10:05:53 +0530

    Completed

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
-from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from scrape import (
+    scrape_website, 
+    split_dom_content, 
+    clean_body_content, 
+    extract_body_content,
+)
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -15,3 +21,13 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Please provide a prompt....")
+
+    if st.button("Parse"):
+        st.write("Parsing.....")
+
+        dom_chunks = split_dom_content(st.session_state.dom_content)
+        result = parse_with_ollama(dom_chunks, parse_description)
+        st.write(result)
